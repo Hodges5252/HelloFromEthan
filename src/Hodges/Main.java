23f27b1c47e570c256cbc42e5ac8c259e695261e
Kimball Newton <new17005@byui.edu>
2022-02-04 18:12:51 -0700

    git commit

--- src/Hodges/Main.java
+++ src/Hodges/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args)
     {
+
         System.out.println("Hello, my name is Ethan!");
+        System.out.println("Hello Ethan, I am the Incredibly Awesome and Unexpectedly Insignificant Kimball.");
     }
 }
